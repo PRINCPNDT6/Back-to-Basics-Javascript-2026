e654225c3f0e4e3cebeb95ec6a78a96ab9c7be82
princpndt6 <krrishsharma698@gmail.com>
2025-02-12 19:35:12 +0530

    conversion of datatype

---
+++ basics.js/1typeof.js
@@ -0,0 +1,55 @@
+let val = null
+ console.log(typeof val); // null = object
+// change in number
+let id = Number(val)
+//  console.log(typeof id);
+console.log(id);// 0
+
+
+let num = 0
+ 
+console.log(typeof num); // 0=number
+// change in number 
+let vat = Number(num)
+// console.log(typeof vat);
+console.log(vat);// 0
+
+
+let value= Boolean
+console.log(typeof value); // boolean = function
+// change in number
+let numb = Number(value)
+//  console.log(typeof numb);
+console.log(numb); //NaN
+
+// let fanc = true
+
+// console.log(typeof fanc); //  true=boolean
+// // change in number
+// let show = Number(fanc)
+// console.log(typeof show); // 
+// console.log(show); // 1
+
+let fanc = false
+
+console.log(typeof fanc); //  true=boolean
+// change in number
+let show = Number(fanc)
+// console.log(typeof show); // 
+console.log(show); // 0
+ 
+ let some = 33
+console.log(typeof some);
+//  change in number
+ let somenumber = String(some)
+console.log(typeof somenumber);
+
+ console.log(somenumber);
+ 
+
+
+
+
+
+
+
