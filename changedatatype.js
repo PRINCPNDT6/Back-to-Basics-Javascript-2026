2838cc5f3112043be2973d2bab27eb36b31a0e08
princpndt6 <krrishsharma698@gmail.com>
2025-02-12 18:07:43 +0530

    change datatype

---
+++ changedatatype.js
@@ -0,0 +1,34 @@
+// String into Number
+let score="44abc"  // in string
+
+console.log(score);
+console.log(typeof score);
+console.log(typeof(score));
+// let change in number into string
+let IdNumber  = Number(score); // in number
+
+console.log(IdNumber);
+console.log(typeof IdNumber);
+console.log(typeof(IdNumber));
+
+ // Number into String
+let num=44  // in number
+
+console.log(num);
+console.log(typeof(num));
+console.log(typeof(num))
+// lets change in string into number
+
+ let val = String(num) // in string
+ console.log(val);
+console.log(typeof val);
+console.log(typeof(val));
+
+
+
+
+
+
+
+
+
