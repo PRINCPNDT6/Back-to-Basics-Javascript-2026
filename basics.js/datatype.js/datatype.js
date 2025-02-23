76661fea3468a5e6f1435ab0fb2313fb5291f352
princpndt6 <krrishsharma698@gmail.com>
2025-02-23 17:01:27 +0530

    datatype js

---
+++ basics.js/datatype.js/datatype.js
@@ -0,0 +1,54 @@
+// A // Prmitive data type
+
+// 7 type of prmitive datatype 
+// NN SS BB U
+// null
+// Number
+// Symbol
+// String
+// Boolean
+// BigInt
+// undefined
+
+// B //  Non prmitive data type(REFERENCE)
+
+// 3 type of non primitive datatype
+// Array
+// Object
+// function
+co
+// example of A
+const score = null
+const scorevalue = 200
+const id = Symbol('1233')
+const IdNumber = Symbol('1233')
+const name = "princpndt"
+const  a = false
+let num = 92892382938n
+let username
+
+console.log(score,scorevalue,id,name,a,IdNumber,num,username);
+
+
+console.log(typeof scorevalue );
+console.log(typeof score );
+console.log(typeof id);
+console.log(typeof name);
+console.log(typeof a);
+console.log(typeof username);
+console.log(typeof IdNumber);
+
+// // example of B 
+// ARRAY
+const Array = ["prince","lorem","html"]
+// OBJECT
+let my Object = {
+    name = "prine"
+    age  = 20;
+    roll = 317;
+}
+// FUNCTION
+const myfunction = function(){
+
+    console.log("hello world")
+}
