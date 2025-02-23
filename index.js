1b577d2624cad42a36da6e256cb09845ad9c158b
princpndt6 <krrishsharma698@gmail.com>
2025-02-23 16:38:11 +0530

    table.js

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// for(i=0;i<=6;i++)
+// console.log("PRINCE SHARMA")
+alert=console.log("PRINCE SHARMA")
+//  constant in javascript
+const  accountId=123454
+let accountEmail = "prince@google.com"
+var accountPass = "1111"
+accountCity="meerut"
+
+// const accountId = 232323 not allowed
+accountEmail = "nikhil@gmail.com"
+accountPass="2222"
+accountCity="baghpat"
+// accountId=123123 constant not to be change
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPass,accountCity]);
